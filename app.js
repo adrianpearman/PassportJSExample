38b9924121206098bd75e0f94f6a7e1ebe7fc237
Adrian Pearman <adrianpearman12@gmail.com>
2019-01-02 12:12:07 -0500

    adding in additional middleware to handle the sending of success messages. updating partials to display the new messages.

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 // NPM Modules
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 
 // EJS Middleware
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 
+// BodyParser
+app.use(express.urlencoded({ extended: false }))
+
+// Express Session Middlewares
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    // cookies: { secure: true }
+}))
+
+// Adding in the flash middleware
+app.use(flash())
+
+// Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
+
 // Environment Variables
 const PORT = process.env.PORT || 3000
+const db = require('./config/keys').MongoURI
+
+// connecting MongoDB
+mongoose.connect(db, { useNewUrlParser: true})
+.then(() => {
+    console.log('Connected to MongoDB')
+})
+.catch(err => console.log(err))
 
 // Routes
 app.use('/', require('./routes/index'))
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,82 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const router = express.Router()
+const User = require('../models/User')
 
 // Login Page
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
-// Login Page
+// Register Page
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const { name, password, password2} = req.body
+    const email = req.body.email.toLowerCase()
+    let errors = []
+
+    // error handling
+    if(!name || !email || !password || !password2){
+        errors.push({ msg: 'Please fill in the fields'})
+    }
+
+    if(password !== password2){
+        errors.push({ msg: 'Passwords do not match'})
+    }
+
+    if(password.length < 7){
+        errors.push({ msg: 'Please enter in a password'})
+    }
+
+    if(errors.length > 0){
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else{
+        // Validation Successful
+        User.findOne({ email: email })
+            .then((user) => {
+                if(user){
+                    errors.push({ msg: 'Email is already registered'})
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    })
+                }else{
+                    const newUser = new User({
+                        name,
+                        email, 
+                        password
+                    })
+
+                    // Hashing the password
+                    bcrypt.genSalt(10, (err, salt) => {
+                        if(err)throw err
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err)throw err
+                            newUser.password = hash
+                            newUser.save()
+                                .then(() => {
+                                    req.flash('success_msg', 'You are now registered and can now log in')
+                                    res.redirect('/users/login')
+                                })
+                                .catch(err => console.log(err))
+                        })
+                    })
+                }
+            })
+            .catch(err => console.log(err))
+    }
 })
 
 module.exports = router
